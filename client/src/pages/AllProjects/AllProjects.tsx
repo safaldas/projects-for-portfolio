b7fcd939a6e9270f61de0b19d4a459cf989e0a90
Rachana <rachana@neoito.com>
2023-08-14 13:10:31 +0530

    all project api

--- client/src/pages/AllProjects/AllProjects.tsx
+++ client/src/pages/AllProjects/AllProjects.tsx
@@ -1,42 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import { Container, ListContainer,Button, ButtonContainer,TextContainer } from './style';
-import { Card, Typography, Pagination } from '@phork/phorkit'
+import { Container, ListContainer, TextContainer } from './style';
+import { Card, Typography, Pagination, StyledLoader } from '@phork/phorkit'
 import Description from '../../components/Description/Description'
-import { useNavigate } from "react-router-dom";
 import ToolBar from '../../components/ToolBar';
 
-import mockCards from '../../data/cards'
-import ViewModel from '../../components/ViewModel/ViewModel'
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "../../util/axios-instance";
 
 const AllProjects = () => {
 
-  const URL = "https://jsonplaceholder.typicode.com/posts";
-  const [data, getData] = useState([]);
+  const [dataContent, getData] = useState([]);
   // const [isAdmin, setisAdmin] = useState(true);
   const [content, setContent] = useState();
 
 
-  const navigateTo = useNavigate();
+  const { data, isLoading } = useQuery({
+    queryKey: ["projects"],
+    queryFn: async () => {
+      const response = await axiosInstance.get("/projects", {
+        params: {
+          page: 1,
+          limit: 10,
+        },
+      });
+      const data = await response.data;
 
-
+      getData(data)
+      return data;
+    },
+  });
 
   useEffect(() => {
-    fetchData();
-    //  console.log(data[0])
-  }, []);
+    // handleProjectApi?.data
+    console.log(dataContent, "dd")
+    console.log(data, "api")
+  }, [dataContent]);
 
-  const fetchData = () => {
-    fetch(URL)
-      .then((res) => res.json())
-
-      .then((response) => {
-        // console.log(response);
-        getData(response);
-      });
-  };
-
-  const arrayElm = (task: React.SetStateAction<undefined>)=>{
-      setContent(task);
+  const arrayElm = (task: React.SetStateAction<undefined>) => {
+    setContent(task);
 
   }
 
@@ -44,96 +45,72 @@ const AllProjects = () => {
   return (
     <>
       <div>
-        <Container>
-          <ToolBar length={data.length} isAdmin />
-          {/* <Header>
-            <h1>All Projects {`(${data.length})`}</h1>
+        {isLoading ? <StyledLoader style={{ marginLeft: "30%" }} color="#556270" /> :
 
-            <Button
-  as="button"
-  onClick={()=>navigateTo('/myProject')}
-  type="button"
-  weight="inline"
-  size='large'
-  style={{fontSize:'20px'}}
->
- My Projects
-</Button>       
-     {isAdmin && <AddProject/>}
-     <Button
-  as="button"
-  onClick={()=>navigateTo('/')}
-  type="button"
-  weight="solid"
-  size='large'
->
- logout
-</Button>    */}
-          {/* </Header> */}
-          <ListContainer>
+          <Container>
+            <ToolBar length={data?.data?.length} isAdmin />
+            <ListContainer>
 
-            {data.map((task) =>
-              <Card
-                hoverable
-                raised={10}
-                key={task?.id}
-                id={task?.id}
-                style={{
-                  marginLeft: "50px",
-                  marginBottom: "50px",
-                  width: "25%"
-                }}
-                onClick={() => setContent(task)}
-              >
-                <TextContainer>
-                <Typography
-                  as="h1"
-                  color="primary"
+              {data?.data?.map((task) =>
+                <Card
+                  hoverable
+                  raised={10}
+                  key={task?.id}
+                  id={task?.id}
                   style={{
-                    alignItems: 'center',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    fontWeight: 'bold'
+                    marginLeft: "50px",
+                    marginBottom: "50px",
+                    width: "25%"
                   }}
+                  onClick={() => setContent(task)}
                 >
-                  {task?.title}
-                </Typography>
-                <Typography
-                  as="div"
-                  color="primary"
-                  style={{
-                    alignItems: 'center',
-                    display: 'flex',
-                    justifyContent: 'center',
-                  }}
-                >
-                  {`${task?.body?.substring(0, 100)}...`}
-                </Typography>
-                </TextContainer>
-                <div onClick={()=>arrayElm(task)}>
-                <Description content={content}/>
+                  <TextContainer>
+                    <Typography
+                      as="h1"
+                      color="primary"
+                      style={{
+                        alignItems: 'center',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        fontWeight: 'bold'
+                      }}
+                    >
+                      {task?.name}
+                    </Typography>
+                    <Typography
+                      as="div"
+                      color="primary"
+                      style={{
+                        alignItems: 'center',
+                        display: 'flex',
+                        justifyContent: 'center',
+                      }}
+                    >
+                      {`${task?.description?.substring(0, 100)}...`}
+                    </Typography>
+                  </TextContainer>
+                  <div onClick={() => arrayElm(task)}>
+                    <Description content={content} />
 
-                </div>
-                {/* <ButtonContainer>
-              {location.pathname==='/myProjects' ?  <Button onClick={()=>navigateTo('/board')}>View</Button> : <ViewModel tasks={task} body={task?.body}/>}  
-                {location.pathname==='/all' && <Button onClick={()=>navigateTo('/board')}>Start</Button>}
-            </ButtonContainer> */}
-              </Card>
-            )}
+                  </div>
+                </Card>
+              )}
 
+
+            </ListContainer>
             <Pagination
               color="primary"
-              justify="start"
+              justify="center"
               onChangePage={function noRefCheck() { }}
               page={8}
               pageLabelProps={{
-                size: 'medium',
+                size: 'large',
                 variants: 'no-wrap'
               }}
               pageLinks={6}
               pageSize={10}
               shape="pill"
-              size="medium"
+              size="large"
               spacing="joined"
               totalItems={300}
               weight="shaded"
@@ -141,9 +118,8 @@ const AllProjects = () => {
               withPageLinks
               withPreviousAndNext
             />
-          </ListContainer>
-          {/* {content && <Description content={content} />} */}
-        </Container>
+          </Container>
+        }
       </div>
     </>
   )
